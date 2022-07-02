07ef09e29757ace272abbe8951dad17cf1508115
Frontend_Oleh_A <oleh.abanin@abz.agency>
2022-07-02 23:07:00 +0300

    chore: install nextjs

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import {AppProps} from "next/dist/shared/lib/router/router";
+import Head from "next/head";
+
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} /></>
+}
+
+export default MyApp
